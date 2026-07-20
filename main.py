b3d056b4a84ea67e725fab6efab31295c15e9b6b
ahora885 <ahoraahmadian9@gmail.com>
2026-07-20 14:35:51 +0330

    Update main.py

--- main.py
+++ main.py
@@ -3,32 +3,62 @@ import asyncio
 from aiogram import Bot, Dispatcher
 
 from config import BOT_TOKEN
+from database import create_tables
 
-from start import router as start_router
-from account import router as account_router
-from wallet import router as wallet_router
-from shop import router as shop_router
-from support import router as support_router
-from admin import router as admin_router
+from handlers import start
+from handlers import account
+from handlers import wallet
+from handlers import shop
+from handlers import support
+from handlers import admin
 
-
-bot = Bot(BOT_TOKEN)
-
-dp = Dispatcher()
-
-
-dp.include_router(start_router)
-dp.include_router(account_router)
-dp.include_router(wallet_router)
-dp.include_router(shop_router)
-dp.include_router(support_router)
-dp.include_router(admin_router)
 
 
 async def main():
-    print("Bot Started...")
-    await dp.start_polling(bot)
+
+    await create_tables()
+
+
+    bot = Bot(
+        token=BOT_TOKEN
+    )
+
+    dp = Dispatcher()
+
+
+    dp.include_router(
+        start.router
+    )
+
+    dp.include_router(
+        account.router
+    )
+
+    dp.include_router(
+        wallet.router
+    )
+
+    dp.include_router(
+        shop.router
+    )
+
+    dp.include_router(
+        support.router
+    )
+
+    dp.include_router(
+        admin.router
+    )
+
+
+    await dp.start_polling(
+        bot
+    )
+
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+
+    asyncio.run(
+        main()
+    )
