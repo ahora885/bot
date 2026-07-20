4a52e394ca41713b107331b169ee6cd7bf639fa9
ahora885 <ahoraahmadian9@gmail.com>
2026-07-20 14:00:48 +0330

    Update database.py

--- database.py
+++ database.py
@@ -29,6 +29,7 @@ AsyncSessionLocal = sessionmaker(
 Base = declarative_base()
 
 
+
 class User(Base):
 
     __tablename__ = "users"
@@ -39,36 +40,43 @@ class User(Base):
         primary_key=True
     )
 
+
     telegram_id = Column(
         Integer,
         unique=True,
         index=True
     )
 
+
     username = Column(
         String,
         nullable=True
     )
+
 
     first_name = Column(
         String,
         nullable=True
     )
 
+
     wallet = Column(
         Integer,
         default=0
     )
+
 
     welcome_bonus = Column(
         Boolean,
         default=False
     )
 
+
     free_test_count = Column(
         Integer,
         default=0
     )
+
 
     configs = Column(
         Integer,
@@ -76,9 +84,50 @@ class User(Base):
     )
 
 
+
+class Order(Base):
+
+    __tablename__ = "orders"
+
+
+    id = Column(
+        Integer,
+        primary_key=True
+    )
+
+
+    telegram_id = Column(
+        Integer,
+        index=True
+    )
+
+
+    volume = Column(
+        Integer
+    )
+
+
+    days = Column(
+        Integer
+    )
+
+
+    price = Column(
+        Integer
+    )
+
+
+    status = Column(
+        String,
+        default="pending"
+    )
+
+
+
 async def create_tables():
 
     async with engine.begin() as conn:
+
         await conn.run_sync(
             Base.metadata.create_all
         )
