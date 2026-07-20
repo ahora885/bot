02d17ad1dcc9c9148bda8c803652b19afb7741cf
ahora885 <ahoraahmadian9@gmail.com>
2026-07-20 13:34:05 +0330

    Update wallet.py

--- wallet.py
+++ wallet.py
@@ -1,19 +1,43 @@
 from aiogram import Router, types
 
+from database import Session, User
+
+
 router = Router()
 
 
 @router.message(lambda message: message.text == "💰 کیف پول")
 async def wallet_handler(message: types.Message):
 
+    db = Session()
+
+    user = db.query(User).filter(
+        User.telegram_id == message.from_user.id
+    ).first()
+
+
+    if not user:
+        await message.answer(
+            "❌ ابتدا ربات را با /start شروع کنید."
+        )
+        db.close()
+        return
+
+
     await message.answer(
-        """
+        f"""
 💰 کیف پول شما
 
 💵 موجودی:
-0 تومان
+{user.wallet} تومان
 
-📜 تاریخچه تراکنش:
-هنوز تراکنشی ندارید.
+📦 تعداد کانفیگ خریداری شده:
+{user.configs}
+
+🧾 وضعیت:
+فعال
 """
     )
+
+
+    db.close()
