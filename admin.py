130eeb6dd01d5d4417ea98a1b914ce793246a566
ahora885 <ahoraahmadian9@gmail.com>
2026-07-20 14:11:59 +0330

    Update admin.py

--- admin.py
+++ admin.py
@@ -1,28 +1,49 @@
 from aiogram import Router, types
-from aiogram.filters import Command
+from sqlalchemy import select, func
 
+from database import AsyncSessionLocal, User, Order
 from config import ADMIN_ID
+
 
 router = Router()
 
 
-@router.message(Command("admin"))
+
+@router.message(lambda message: message.text == "👨‍💻 پنل ادمین")
 async def admin_panel(message: types.Message):
 
     if message.from_user.id != ADMIN_ID:
-        await message.answer("⛔ دسترسی ندارید.")
+
+        await message.answer(
+            "❌ شما دسترسی ادمین ندارید."
+        )
+
         return
 
+
+    async with AsyncSessionLocal() as session:
+
+        users = await session.execute(
+            select(func.count(User.id))
+        )
+
+        orders = await session.execute(
+            select(func.count(Order.id))
+        )
+
+
+        user_count = users.scalar()
+        order_count = orders.scalar()
+
+
     await message.answer(
-        """
+        f"""
 👨‍💻 پنل مدیریت
 
-امکانات در حال آماده‌سازی:
+👥 تعداد کاربران:
+{user_count}
 
-👥 کاربران
-📦 کانفیگ‌ها
-💰 کیف پول‌ها
-📊 آمار
-📢 ارسال همگانی
+📦 تعداد سفارش‌ها:
+{order_count}
 """
     )
