750e9612dd71f6b9cb5aa6b693cfa821bf7f6274
ahora885 <ahoraahmadian9@gmail.com>
2026-07-20 13:26:50 +0330

    Update database.py

--- database.py
+++ database.py
@@ -1,31 +1,69 @@
 from sqlalchemy import create_engine, Column, Integer, String, Boolean
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-engine = create_engine("sqlite:///bot.db")
+
+engine = create_engine(
+    "sqlite:///bot.db"
+)
+
 
 Base = declarative_base()
 
-Session = sessionmaker(bind=engine)
+Session = sessionmaker(
+    bind=engine
+)
 
 
 class User(Base):
+
     __tablename__ = "users"
 
-    id = Column(Integer, primary_key=True)
 
-    telegram_id = Column(Integer, unique=True)
+    id = Column(
+        Integer,
+        primary_key=True
+    )
 
-    username = Column(String)
 
-    first_name = Column(String)
+    telegram_id = Column(
+        Integer,
+        unique=True
+    )
 
-    wallet = Column(Integer, default=0)
 
-    welcome_bonus = Column(Boolean, default=False)
+    username = Column(
+        String
+    )
 
-    free_test_count = Column(Integer, default=0)
 
-    configs = Column(Integer, default=0)
+    first_name = Column(
+        String
+    )
+
+
+    wallet = Column(
+        Integer,
+        default=0
+    )
+
+
+    welcome_bonus = Column(
+        Boolean,
+        default=False
+    )
+
+
+    free_test_count = Column(
+        Integer,
+        default=0
+    )
+
+
+    configs = Column(
+        Integer,
+        default=0
+    )
+
 
 
 Base.metadata.create_all(engine)
