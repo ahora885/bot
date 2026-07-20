ef4b25779ee71a103849c395269226f158f4ecab
ahora885 <ahoraahmadian9@gmail.com>
2026-07-20 13:37:22 +0330

    Update shop.py

--- shop.py
+++ shop.py
@@ -1,6 +1,39 @@
 from aiogram import Router, types
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 router = Router()
+
+
+shop_menu = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [
+            InlineKeyboardButton(
+                text="5 گیگ",
+                callback_data="volume_5"
+            ),
+            InlineKeyboardButton(
+                text="10 گیگ",
+                callback_data="volume_10"
+            )
+        ],
+        [
+            InlineKeyboardButton(
+                text="20 گیگ",
+                callback_data="volume_20"
+            ),
+            InlineKeyboardButton(
+                text="50 گیگ",
+                callback_data="volume_50"
+            )
+        ],
+        [
+            InlineKeyboardButton(
+                text="100 گیگ",
+                callback_data="volume_100"
+            )
+        ]
+    ]
+)
 
 
 @router.message(lambda message: message.text == "📦 خرید کانفیگ")
@@ -10,20 +43,31 @@ async def shop_handler(message: types.Message):
         """
 📦 خرید کانفیگ
 
-لطفاً حجم و مدت زمان کانفیگ را انتخاب کنید.
-
-حجم‌ها:
-5GB
-10GB
-20GB
-50GB
-100GB
-
-مدت:
-7 روز تا 3 ماه
+حجم مورد نظر را انتخاب کنید:
 
 💰 قیمت:
-هر گیگ = 2000 تومان
-هر روز = 1000 تومان
+هر گیگ = ۲۰۰۰ تومان
+هر روز = ۱۰۰۰ تومان
+""",
+        reply_markup=shop_menu
+    )
+
+
+@router.callback_query(lambda call: call.data.startswith("volume_"))
+async def volume_select(call: types.CallbackQuery):
+
+    volume = call.data.replace(
+        "volume_",
+        ""
+    )
+
+    await call.message.answer(
+        f"""
+✅ حجم انتخاب شد:
+{volume} گیگ
+
+حالا مدت زمان را انتخاب کنید.
 """
     )
+
+    await call.answer()
